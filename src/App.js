27b57342b05b06e3879a17bb257f90db4f569e3b
Szkrishna <krisnas9792@gmail.com>
2024-12-28 23:34:04 +0530

    code for upper sidebar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import './App.css';
 
 function App() {
   return (
-    <>
-    
-    </>
+    <div className='app'>
+      <div className='sidebar'>
+        <div className='upperSide'>
+          <div className='upperSideTop'>
+            <img src='' alt='' /><span className='brand'>ChatGPT</span>
+            <button className='midBtn'><img src='' alt='' /><span className='addBtn'>ChatGPT</span>New Chat</button>
+            <div className='upperSideBottom'>
+              <div className='query'><img src='' alt='' />What is Programming ?</div>
+              <div className='query'><img src='' alt='' />What is Programming ?</div>
+            </div>
+          </div>
+        </div>
+        <div className='lowerSide'>
+
+        </div>
+      </div>
+      <div className='main'></div>
+    </div>
   );
 }
 
