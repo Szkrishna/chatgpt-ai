681fce8a76e714011f05f9147378e7fd4d9d6d49
Szkrishna <krisnas9792@gmail.com>
2024-12-29 18:42:45 +0530

    adding img to the sidebar and styling uppside top and mid button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
-import gptLogo from "./assets/chatgpt.svg"
+import gptLogo from "./assets/chatgpt.svg";
+import addBtn from "./assets/add-30.png";
+import msgIcon from "./assets/message.svg";
+import home from "./assets/home.svg";
+import saved from "./assets/bookmark.svg";
+import rocket from "./assets/rocket.svg";
 
 function App() {
   return (
@@ -7,16 +12,24 @@ function App() {
       <div className='sideBar'>
         <div className='upperSide'>
           <div className='upperSideTop'>
-            <img src={gptLogo} alt='Logo' /><span className='brand'>ChatGPT</span>
-            <button className='midBtn'><img src='' alt='' /><span className='addBtn'></span>New Chat</button>
-            <div className='upperSideBottom'>
-              <div className='query'><img src='' alt='' />What is Programming ?</div>
-              <div className='query'><img src='' alt='' />What is Programming ?</div>
-            </div>
+            <img src={gptLogo} alt='Logo' className='logo'/><span className='brand'>ChatGPT</span>
+          </div>
+          <button className='midBtn'><img src={addBtn} alt='new chat' /><span className='addBtn'></span>New Chat</button>
+          <div className='upperSideBottom'>
+            <div className='query'><img src={msgIcon} alt='Query' />What is Programming ?</div>
+            <div className='query'><img src={msgIcon} alt='Query' />How to use an API ?</div>
           </div>
         </div>
         <div className='lowerSide'>
-
+          <div className='listItems'>
+            <img src={home} alt='home' className='listItemsImg' />Home
+          </div>
+          <div className='listItems'>
+            <img src={saved} alt='saved' className='listItemsImg' />saved
+          </div>
+          <div className='listItems'>
+            <img src={rocket} alt='rocket' className='listItemsImg' />Upgrade to Pro
+          </div>
         </div>
       </div>
       <div className='main'></div>
